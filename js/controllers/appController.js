169a29e77cef0552785df53f093607eaafdd8ac3
João Leonardo Carmo <joao.carmo@webdetails.pt>
2014-05-02 14:58:49 +0100

    - Added simple controller action.

--- js/controllers/appController.js
+++ js/controllers/appController.js
@@ -2,6 +2,15 @@
 
 app.controller('appController',
     function ($scope) {
+
+        //VM initialization
         $scope.hello = "Hello World!";
+        $scope.count = 0;
+
+        //application logic
+        $scope.addCount = function () {
+            //this could be a service call to execute business logic
+            $scope.count++;
+        }
     }
 );
