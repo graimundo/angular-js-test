9941f19a42425330ce5df900d1a9b4e38657ba50
João Leonardo Carmo <joao.carmo@webdetails.pt>
2014-05-05 15:08:42 +0100

    - Renamed index.html to main.html - Added CORS support for angular requests (commented)

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 'use strict';
 
 var app = angular.module('app', ['ngResource']);
+
+//enable CORS in Angular http requests
+/*app.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}]);*/
