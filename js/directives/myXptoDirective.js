b9c674c4976fca87c31b04a45775b45e4fc52e76
João Leonardo Carmo <joao.carmo@webdetails.pt>
2014-05-06 17:30:40 +0100

    - minor cleaning.

--- js/directives/myXptoDirective.js
+++ js/directives/myXptoDirective.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.directive('myXpto', function($compile) {
+app.directive('myXpto', function() {
     return {
         restrict: 'E',
         template: "<input type='text' data-ng-model='sampleData'/>{{sampleData}}<br/>"
